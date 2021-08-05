30025e510239e84d799a1a4741d7c509b3e4a615
Ambiorix <pabloa.bonilla@outlook.com>
2021-08-05 17:13:14 -0400

    3rd upload

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-const APIURL =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const IMGPATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI =
-    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
-
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-// initially get fav movies
-getMovies(APIURL);
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6193a0db431493626cef95cea16d4d9d&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-async function getMovies(url) {
-    const resp = await fetch(url);
-    const respData = await resp.json();
 
-    console.log(respData);
-
-    showMovies(respData.results);
+async function fetchMovies(url) {
+    let resp = await fetch(url);
+    let respData = await resp.json();
+    console.log(resp)
+        
+    showMovies(respData.results)
 }
 
+
+fetchMovies(APIURL);
+
 function showMovies(movies) {
-    // clear main
+    
     main.innerHTML = "";
 
     movies.forEach((movie) => {
-        const { poster_path, title, vote_average, overview } = movie;
+        let { poster_path, title, vote_average, overview } = movie;
+        
+        if(poster_path === null) {
+            poster_path = "/A31EtMx1Jk1p4Si3d3EnCVHuqyM.jpg";
+        } else {
+           poster_path
+        }
 
         const movieEl = document.createElement("div");
         movieEl.classList.add("movie");
@@ -67,8 +73,9 @@ form.addEventListener("submit", (e) => {
     const searchTerm = search.value;
 
     if (searchTerm) {
-        getMovies(SEARCHAPI + searchTerm);
+        fetchMovies(SEARCHAPI + searchTerm);
 
         search.value = "";
     }
 });
+
